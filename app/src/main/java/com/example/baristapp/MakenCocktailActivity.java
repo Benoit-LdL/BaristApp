5baff324e66f99b913944f8560ca456f5524db7f
Maximilian Senten <maximilian.senten00@gmail.com>
2019-11-22 15:00:41 +0100

    Activities aangemaakt

---
+++ app/src/main/java/com/example/baristapp/MakenCocktailActivity.java
@@ -0,0 +1,14 @@
+package com.example.baristapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MakenCocktailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maken);
+    }
+}
