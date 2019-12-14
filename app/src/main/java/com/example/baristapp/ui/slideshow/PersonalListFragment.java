3dbf996de8dfdd30ef9410cfeddb0cb2b18cef9d
Maximilian Senten <maximilian.senten00@gmail.com>
2019-12-14 15:28:46 +0100

    aanpassingen menu

--- app/src/main/java/com/example/baristapp/ui/slideshow/PersonalListFragment.java
+++ app/src/main/java/com/example/baristapp/ui/slideshow/PersonalListFragment.java
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.baristapp.R;
 
-public class SlideshowFragment extends Fragment {
+public class PersonalListFragment extends Fragment {
 
-    private SlideshowViewModel slideshowViewModel;
+    private PersonalListViewModel personalListViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        slideshowViewModel =
-                ViewModelProviders.of(this).get(SlideshowViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
+        personalListViewModel =
+                ViewModelProviders.of(this).get(PersonalListViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_personallist, container, false);
         final TextView textView = root.findViewById(R.id.text_slideshow);
-        slideshowViewModel.getText().observe(this, new Observer<String>() {
+        personalListViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
